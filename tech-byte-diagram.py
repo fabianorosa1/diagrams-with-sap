7ca4272b062f2ed3bc491c336cb5e6bf17bbceee
Fabiano Rosa <fabianorosa1@gmail.com>
2024-06-18 16:46:22 +0200

    Update tech-byte-diagram.py

--- tech-byte-diagram.py
+++ tech-byte-diagram.py
@@ -1,10 +1,10 @@
 # diagram.py
 from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.storage import S3
-from diagrams.sap.other import PlaceholderNewServices_Circled
-from diagrams.sap.integration import IntegrationSuite_Circled
-from diagrams.sap.database_datamanagement import SAPHANACloud_Circled
-from diagrams.sap.database_datamanagement import ObjectStore_Circled
+from diagrams.sap.other import PlaceholderNewServices
+from diagrams.sap.integration import IntegrationSuite
+from diagrams.sap.database_datamanagement import SAPHANACloud
+from diagrams.sap.database_datamanagement import ObjectStore
 
 # SAP BTP Solution Diagrams and Icons guidelines colours
 L0_BLUE_COLOUR = "#0070F2"
@@ -21,11 +21,11 @@ NON_SAP_AREA_COLOUR = "#595959"
 with Diagram("SAP Tech Byte - Exploring the SAP Audit Log service - Guidelines Colours", show=False):
     with Cluster("SAP Business Technology Platform", graph_attr= {"bgcolor": L0_FILLED_COLOUR, "pencolor": L0_BLUE_COLOUR}):
         with Cluster("Subaccount", graph_attr= {"bgcolor": "white", "pencolor": L1_BLUE_COLOUR}):
-            cloud_integration = IntegrationSuite_Circled("Cloud Integration")
-            object_store = ObjectStore_Circled("Object Store")
+            cloud_integration = IntegrationSuite("Cloud Integration")
+            object_store = ObjectStore("Object Store")
 
-            PlaceholderNewServices_Circled("Audit Log service") << Edge(label="Retrieves entries", color=FIX_GREY_COLOUR) << \
-            cloud_integration >> Edge(color=FIX_GREY_COLOUR) >> SAPHANACloud_Circled("HANA Cloud")
+            PlaceholderNewServices("Audit Log service") << Edge(label="Retrieves entries", color=FIX_GREY_COLOUR) << \
+            cloud_integration >> Edge(color=FIX_GREY_COLOUR) >> SAPHANACloud("HANA Cloud")
             cloud_integration >> Edge(color=FIX_GREY_COLOUR) >> object_store
         
     with Cluster("AWS", graph_attr= {"bgcolor": "white", "pencolor": NON_SAP_AREA_COLOUR}):
